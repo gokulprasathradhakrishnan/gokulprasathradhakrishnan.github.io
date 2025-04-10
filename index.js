b3fb80941c3c11711b808e35c135883cfbdfdd44
Gokul Prasath Radhakrishnan <gokulprasath1609@gmail.com>
2025-04-10 19:09:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -42,3 +42,15 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const accordions = document.querySelectorAll(".accordion");
+
+  accordions.forEach((accordion) => {
+    accordion.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const panel = this.nextElementSibling;
+      panel.style.display = panel.style.display === "block" ? "none" : "block";
+    });
+  });
+});
